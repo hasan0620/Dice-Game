897aa832c880bd4d5590fcd22417bfd8817ebad5
Yuhei Shirahata <s1320144@u-aizu.ac.jp>
2025-06-27 11:49:53 +0900

    Dice game: initial revision

---
+++ Dice.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+class Dice {
+    public static void main(String args[]) {
+	Random rand = new Random();
+	int a = rand.nextInt(10) + 1;
+	int b = rand.nextInt(10) + 1;
+	int sum = a + b;
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: " + a);
+	System.out.println("Die 2: " + b);
+	System.out.println("Total value: " + sum);
+    }
+}
